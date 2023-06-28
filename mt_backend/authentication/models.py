36d5d94aeba07f95b81dae3fbff1bb423b68c296
SudiCodes <eee.ee166480@silicon.ac.in>
2023-06-28 16:47:08 +0530

    added Jenkinsfile

--- mt_backend/authentication/models.py
+++ mt_backend/authentication/models.py
@@ -34,25 +34,26 @@ class Customer(AbstractUser):
     objects = CustomUserManager()
 
     def save(self, *args, **kwargs):
-        base_name = "{}.{}".format(
-            self.first_name.lower(), self.last_name.lower())
-        username = base_name
-        counter = 1
-        while Customer.objects.filter(username=username).exists():
-            username = "{}{}".format(base_name, counter)
-            counter += 1
-        self.username = username
+        if not self.pk:
+            base_name = "{}.{}".format(
+                self.first_name.lower(), self.last_name.lower())
+            username = base_name
+            counter = 1
+            while Customer.objects.filter(username=username).exists():
+                username = "{}{}".format(base_name, counter)
+                counter += 1
+            self.username = username
 
-        user_email = self.email
-        hostname = socket.gethostname()
-        ip = socket.gethostbyname(hostname)
-        # send mail with creds
-        print("Sending mail on success...")
-        send_mail(
-            f'Superuser created - {username}',
-            f'A Superuser has been created with below details:\n\n\n\n\n\temail: {user_email}\n\tpassword: {os.environ.get("DJANGO_SUPERUSER_PASSWORD")}\n\thostname: {hostname}\n\tIPv4 Address: {ip}\n\n\n',
-            'send4sudi@outlook.in',
-            ['sudiptasamalwhy@gmail.com'],
-            fail_silently=False,
-        )
-        super().save(*args, **kwargs)
+            user_email = self.email
+            hostname = socket.gethostname()
+            ip = socket.gethostbyname(hostname)
+            # send mail with creds
+            print("Sending mail on success...")
+            send_mail(
+                f'Superuser created - {username}',
+                f'A Superuser has been created with below details:\n\n\n\n\n\temail: {user_email}\n\tpassword: {os.environ.get("DJANGO_SUPERUSER_PASSWORD")}\n\thostname: {hostname}\n\tIPv4 Address: {ip}\n\n\n',
+                'send4sudi@outlook.in',
+                ['sudiptasamalwhy@gmail.com'],
+                fail_silently=False,
+            )
+            super().save(*args, **kwargs)
