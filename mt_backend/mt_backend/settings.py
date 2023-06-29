12ff2a94c2e738c69dfddf2eed672ea6fc7d661c
SudiCodes <eee.ee166480@silicon.ac.in>
2023-06-29 15:45:54 +0530

    test: checking webhook

--- mt_backend/mt_backend/settings.py
+++ mt_backend/mt_backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from mt_backend.be_secrets import smtp, database
-import os
+# import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
